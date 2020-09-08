8c6bfec8d095f2fbc4ed28b8d0e0a9c604b9f483
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-09-09 00:19:32 +0300

    take a screenshot of a specific area

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/impzero/gou
+
+go 1.14
+
+require (
+	github.com/go-vgo/robotgo v0.90.2
+	github.com/kbinani/screenshot v0.0.0-20191211154542-3a185f1ce18f
+)
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"os"
+	"strconv"
+
+	"github.com/kbinani/screenshot"
+)
+
 // Colors
 const (
 	Blue   = "#63c7ff"
@@ -11,3 +21,46 @@ const (
 	Gray   = "#8c8a8c"
 	White  = "#ffffff"
 )
+
+func main() {
+	x0, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	y0, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		panic(err)
+	}
+	w, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		panic(err)
+	}
+	h, err := strconv.Atoi(os.Args[4])
+	if err != nil {
+		panic(err)
+	}
+
+	x1 := w + x0
+	y1 := h + y0
+
+	// bounds := screenshot.GetDisplayBounds(0)
+	bounds := image.Rect(x0, y0, x1, y1)
+
+	img, err := screenshot.CaptureRect(bounds)
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Create("pesho.png")
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+	png.Encode(file, img)
+
+	fmt.Printf("#%d : %v \"%s\"\n", 0, bounds, "pesho.png")
+
+	fmt.Println(bounds)
+	// fmt.Println(img)
+}
