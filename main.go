47fd96d9d90cb572a2fd16a7e32ffbf1836a2d7a
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-09-08 22:54:24 +0300

    declare all colors

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+// Colors
+const (
+	Blue   = "#63c7ff"
+	Yellow = "#f7ef39"
+	Purple = "#bd75ff"
+	Orange = "#ff8221"
+	Green  = "#42f331"
+	Pink   = "#ff82b5"
+	Gray   = "#8c8a8c"
+	White  = "#ffffff"
+)
